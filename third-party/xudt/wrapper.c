374724b466c3de3c7bddd7b795a8b44e1473ef02
Cedric <14017092+douyixuan@users.noreply.github.com>
2024-07-17 17:25:26 +0800

    xudt: add loading script function

--- third-party/xudt/wrapper.c
+++ third-party/xudt/wrapper.c
@@ -1,6 +1,46 @@
 #include "ckb_syscalls.h"
 #include "molecule/blockchain.h"
 
+
+enum ErrorCode {
+  // 0 is the only success code. We can use 0 directly.
+
+  // inherit from simple_udt
+  ERROR_ARGUMENTS_LEN = -1,
+  ERROR_ENCODING = -2,
+  ERROR_SYSCALL = -3,
+  ERROR_SCRIPT_TOO_LONG = -21,
+  ERROR_OVERFLOWING = -51,
+  ERROR_AMOUNT = -52,
+
+  // error code is starting from 40, to avoid conflict with
+  // common error code in other scripts.
+  ERROR_CANT_LOAD_LIB = 40,
+  ERROR_CANT_FIND_SYMBOL,
+  ERROR_INVALID_RCE_ARGS,
+  ERROR_NOT_ENOUGH_BUFF,
+  ERROR_INVALID_FLAG,
+  ERROR_INVALID_ARGS_FORMAT,
+  ERROR_INVALID_WITNESS_FORMAT,
+  ERROR_INVALID_MOL_FORMAT,
+  ERROR_BLAKE2B_ERROR,
+  ERROR_HASH_MISMATCHED,
+  ERROR_RCRULES_TOO_DEEP,  // 50
+  ERROR_TOO_MANY_RCRULES,
+  ERROR_RCRULES_PROOFS_MISMATCHED,
+  ERROR_SMT_VERIFY_FAILED,
+  ERROR_RCE_EMERGENCY_HALT,
+  ERROR_NOT_VALIDATED,
+  ERROR_TOO_MANY_LOCK,
+  ERROR_ON_BLACK_LIST,
+  ERROR_ON_BLACK_LIST2,
+  ERROR_NOT_ON_WHITE_LIST,
+  ERROR_TYPE_FREEZED,  // 60
+  ERROR_APPEND_ONLY,
+  ERROR_EOF,
+  ERROR_TOO_LONG_PROOF,
+};
+
 // We are limiting the script size loaded to be 32KB at most. This should be
 // more than enough. We are also using blake2b with 256-bit hash here, which is
 // the same as CKB.
@@ -262,6 +302,7 @@ void syscall_exit(int8_t code)
 
 // xUDT
 const uint32_t XUDT_FLAG_SIZE = 4;
+const uint32_t MAX_LOCK_SCRIPT_HASH_COUNT = 2048; // maximum inputs
 typedef struct
 {
   int64_t size;
@@ -311,7 +352,54 @@ mol_seg_t parse_xudt_args_as_mol_seg()
   return args;
 }
 
+// get xudt args raw bytes
 String parse_args() {
   mol_seg_t args = parse_xudt_args_as_mol_seg();
   return mol_seg_to_string(&args);
+}
+
+typedef struct {
+  uint32_t len;
+  uint32_t cap;
+  uint32_t offset;
+  uint8_t* bytes;
+} ByteSlice;
+ByteSlice make_byte_slice(uint32_t len, uint32_t cap, uint32_t offset, uint8_t* ptr) {
+  ByteSlice bs;
+  bs.len = len;
+  bs.cap = cap;
+  bs.bytes = ptr;
+  return bs;
+}
+// load hashes from all inputs cells
+ByteSlice lock_scripts() {
+  uint32_t hashes_count = 0;
+  uint8_t *hashes = malloc(sizeof(uint8_t) * BLAKE2B_BLOCK_SIZE * MAX_LOCK_SCRIPT_HASH_COUNT);
+  memset(hashes, 0, 0);
+  ByteSlice bs = make_byte_slice(0, 0, 0, NULL);
+  // collect hashes
+  size_t i = 0;
+  while (1) {
+    uint8_t buffer[BLAKE2B_BLOCK_SIZE];
+    uint64_t len = BLAKE2B_BLOCK_SIZE;
+    int err = ckb_checked_load_cell_by_field(buffer, &len, 0, i, CKB_SOURCE_INPUT,
+                                         CKB_CELL_FIELD_LOCK_HASH);
+    if (err == CKB_INDEX_OUT_OF_BOUND) {
+      break;
+    }
+    if (err != 0) {
+      return bs;
+    }
+    if (hashes_count >= MAX_LOCK_SCRIPT_HASH_COUNT) {
+      ckb_exit(ERROR_TOO_MANY_LOCK);
+      return bs;
+    }
+
+    memcpy(hashes + hashes_count * BLAKE2B_BLOCK_SIZE, buffer,
+           BLAKE2B_BLOCK_SIZE);
+    hashes_count += 1;
+    i += 1;
+  }
+  // make a string slice
+  return make_byte_slice(hashes_count * BLAKE2B_BLOCK_SIZE, hashes_count * BLAKE2B_BLOCK_SIZE, 0, hashes);
 }
