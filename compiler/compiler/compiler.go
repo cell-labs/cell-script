ab3d34fb21d24d43dfac0f5c804e90929759c137
Cedric <14017092+douyixuan@users.noreply.github.com>
2024-08-30 22:15:38 +0800

    add native header for ckb syscalls

--- compiler/compiler/compiler.go
+++ compiler/compiler/compiler.go
@@ -205,6 +205,7 @@ func (c *Compiler) addGlobal() {
 
 	global.DefinePkgType("bool", types.Bool)
 	global.DefinePkgType("int", types.I64) // TODO: Size based on arch
+	global.DefinePkgType("uint", types.U64) // TODO: Size based on arch
 	global.DefinePkgType("int8", types.I8)
 	global.DefinePkgType("uint8", types.U8)
 	global.DefinePkgType("int16", types.I16)
