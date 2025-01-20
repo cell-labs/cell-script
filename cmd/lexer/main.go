cd10ec0cb37ca0a017cf0cd3d665e425eb2fbc4e
Cedric <14017092+douyixuan@users.noreply.github.com>
2025-01-20 10:50:18 +0800

    Revert "fix compilation error"
    This reverts commit 6a00e7f8bfdd6a50870f387b1c067909310be0d3.

---
+++ cmd/lexer/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/cell-labs/cell-script/internal/compiler"
+)
+
+func init() {
+	compiler.SetupOptions()
+}
+
+func main() {
+	options := compiler.ParseOptions()
+	options.Stage = compiler.STAGE_LEXER
+	err := compiler.Run(options)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
